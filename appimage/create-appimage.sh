65a1309a2714fe37e20c68188dec2cdb1841caed
Patrick McGuire <insidenothing@gmail.com>
2022-01-19 19:06:31 -0500

    Happy 2022

---
+++ appimage/create-appimage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if ! test -f "linuxdeploy-x86_64.AppImage"; then
+    wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+fi
+if ! test -f "linuxdeploy-plugin-qt-x86_64.AppImage"; then
+    wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
+fi
+chmod u+x ./*.AppImage
+cp ../build/release/bmorecoin .
+mkdir -p usr/share/icons
+mkdir -p usr/lib
+cp ../src/images/cryptonote.png usr/share/icons/bmorecoin.png
+./linuxdeploy-x86_64.AppImage --executable ./bmorecoin --desktop-file bmorecoin-desktop.desktop --appdir . --output appimage --plugin qt
